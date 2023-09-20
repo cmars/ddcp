661b7044f2a99361d3b5a379a23451dab7d7f748
Casey Marshall <git@cmars.tech>
2023-09-19 21:28:33 -0500

    Develop with nix flakes, upgrade to Veilid 0.3.2
    Unable to source veilid-core 0.3.2 dependency through crates.io
    directly; the build scripts fail.
    
    Veilid 0.3.2 requires newer versions of capnproto and protobuf tooling
    to be installed in order to compile, so this adds a Nix flake which
    sources those dependencies into a `nix develop` shell.
    
    Veilid 0.3.2 sources are themselves nix-packaged and vendored by
    a symlink to the nix derivation.
    
    This vendored link is easily replaced in a containerized Debian-based
    build, when we're ready for Docker and binary builds for other platforms.

---
+++ flake.nix
@@ -0,0 +1,112 @@
+{
+  description = "velouria";
+
+  inputs = {
+    nixpkgs.url = "nixpkgs";
+
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.flake-utils.follows = "flake-utils";
+    };
+  };
+
+  outputs = { self, nixpkgs, flake-utils, rust-overlay }:
+    (flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [
+            rust-overlay.overlays.default
+          ];
+        };
+
+        arch = "x86_64";  # TODO: derive this from system?
+        capnprotoVersion = "1.0.1";
+        protobufVersion = "24.3";
+        veilidVersion = "0.2.3";
+
+        capnproto_veilid = (with pkgs; stdenv.mkDerivation {
+          pname = "capnproto";
+          version = "${capnprotoVersion}";
+          src = fetchzip {
+            url = "https://capnproto.org/capnproto-c++-${capnprotoVersion}.tar.gz";
+            hash = "sha256-9y1T1ddGEK8X8qjnh5ZDfqgyPagYghJG+EAwdT83VJs=";
+          };
+          nativeBuildInputs = [
+            autoconf
+            automake
+            m4
+            gnumake
+            clang
+          ];
+          buildPhase = "./configure --without-openssl --prefix=$out && make";
+          installPhase = ''
+            mkdir -p $out/bin
+            make -C $TMP/source install prefix=$out
+          '';
+        });
+
+        protobuf_veilid = (with pkgs; stdenv.mkDerivation {
+          pname = "protobuf";
+          version = "${protobufVersion}";
+          src = fetchzip {
+            stripRoot = false;
+            url = "https://github.com/protocolbuffers/protobuf/releases/download/v${protobufVersion}/protoc-${protobufVersion}-linux-${arch}.zip";
+            hash = "sha256-QrJi/s6DEPbX/1k64UFJxzq7SUwapDjuWU5UNrAFMlI=";
+          };
+          buildPhase = ''
+            chmod +x bin/*
+          '';
+          installPhase = ''
+            mkdir -p $out/bin $out/include
+            cp -r $TMP/source/bin/* $out/bin
+            cp -r $TMP/source/include/* $out/include
+          '';
+        });
+
+        veilid_src = (with pkgs; stdenv.mkDerivation {
+          pname = "veilid";
+          version = "${veilidVersion}";
+          src = fetchgit {
+            url = "https://gitlab.com/veilid/veilid.git";
+            rev = "v${veilidVersion}";
+            hash = "sha256-fpA0JsBp2mlyDWlwE6xgyX5KNI2FSypO6m1J9BI+Kjs=";
+            fetchSubmodules = true;
+          };
+          buildPhase = ''
+            ls
+          '';
+          installPhase = ''
+            mkdir -p $out
+            tar cf - . | (cd $out; tar xf -)
+          '';
+        });
+
+      in {
+
+        devShells.default = pkgs.mkShell {
+          buildInputs = [
+            capnproto_veilid
+            protobuf_veilid
+            veilid_src
+          ] ++ (with pkgs; [
+            cargo
+            cargo-watch
+            rustfmt
+            rust-analyzer
+            clang
+            llvmPackages.llvm
+            llvmPackages.libclang
+            gnumake
+            sqlite
+          ]);
+
+          VEILID_SRC="${veilid_src}";
+
+          shellHook = ''
+          '';
+        };
+      }
+    ));
+}
