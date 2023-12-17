f104008fc883ae313bad672f54e6e70e8da7a163
Casey Marshall <git@cmars.tech>
2023-12-17 10:22:34 -0600

    Update protocol crypto to use dh

--- src/proto/crypto.rs
+++ src/proto/crypto.rs
@@ -1,6 +1,9 @@
 use std::array::TryFromSliceError;
 
-use veilid_core::{CryptoSystemVersion, Nonce, SharedSecret, VeilidAPIError, NONCE_LENGTH};
+use veilid_core::{
+    CryptoSystemVersion, Envelope, EnvelopeVersion, Nonce, SharedSecret, Timestamp, TypedKey,
+    TypedKeyPair, VeilidAPIError, NONCE_LENGTH, VALID_ENVELOPE_VERSIONS,
+};
 
 use crate::proto::codec;
 
@@ -19,30 +22,44 @@ pub type Result<T> = std::result::Result<T, Error>;
 
 pub struct Crypto {
     crypto_system: CryptoSystemVersion,
-    secret_key: SharedSecret,
+    sender_key_pair: TypedKeyPair,
+    recipient_public_key: TypedKey,
 }
 
 impl Crypto {
-    pub fn new(crypto_system: CryptoSystemVersion, secret_key: SharedSecret) -> Crypto {
+    pub fn new(
+        crypto_system: CryptoSystemVersion,
+        sender_key_pair: TypedKeyPair,
+        recipient_public_key: TypedKey,
+    ) -> Crypto {
         Crypto {
             crypto_system,
-            secret_key,
+            sender_key_pair,
+            recipient_public_key,
         }
     }
 
     pub fn encode<T: Encodable>(&self, item: T) -> Result<Vec<u8>> {
         let mut message = item.encode()?;
+        let shared_key = self.crypto_system.cached_dh(
+            &self.recipient_public_key.value,
+            &self.sender_key_pair.value.secret,
+        )?;
         let nonce = self.crypto_system.random_nonce();
         self.crypto_system
-            .encrypt_in_place_aead(&mut message, &nonce, &self.secret_key, None)?;
+            .encrypt_in_place_aead(&mut message, &nonce, &shared_key, None)?;
         Ok(vec![nonce.to_vec(), message].concat())
     }
 
     pub fn decode<T: Decodable>(&self, message: &[u8]) -> Result<T> {
         let nonce = Nonce::new(message[0..NONCE_LENGTH].try_into()?);
         let mut message = message[NONCE_LENGTH..].to_vec();
+        let shared_key = self.crypto_system.cached_dh(
+            &self.recipient_public_key.value,
+            &self.sender_key_pair.value.secret,
+        )?;
         self.crypto_system
-            .decrypt_in_place_aead(&mut message, &nonce, &self.secret_key, None)?;
+            .decrypt_in_place_aead(&mut message, &nonce, &shared_key, None)?;
         let result = T::decode(message.as_slice())?;
         Ok(result)
     }
@@ -50,7 +67,7 @@ impl Crypto {
 
 #[cfg(test)]
 mod tests {
-    use veilid_core::CRYPTO_KIND_VLD0;
+    use veilid_core::{CryptoTyped, CRYPTO_KIND_VLD0};
 
     use crate::proto::codec::Request;
     use crate::tests::api::{setup_api, teardown_api, TEST_API_MUTEX};
@@ -67,11 +84,36 @@ mod tests {
             .expect("crypto")
             .get(CRYPTO_KIND_VLD0)
             .expect("vld0");
-        let crypto = Crypto::new(crypto_system.clone(), crypto_system.random_shared_secret());
-        let msg_bytes = crypto.encode(Request::Status).expect("encode");
-        let decoded = crypto.decode::<Request>(&msg_bytes).expect("ok");
+
+        let alice_keypair = CryptoTyped {
+            kind: CRYPTO_KIND_VLD0,
+            value: crypto_system.generate_keypair(),
+        };
+        let bob_keypair = CryptoTyped {
+            kind: CRYPTO_KIND_VLD0,
+            value: crypto_system.generate_keypair(),
+        };
+
+        let alice_crypto = Crypto::new(
+            crypto_system.clone(),
+            alice_keypair,
+            CryptoTyped {
+                kind: bob_keypair.kind,
+                value: bob_keypair.value.key,
+            },
+        );
+        let bob_crypto = Crypto::new(
+            crypto_system.clone(),
+            bob_keypair,
+            CryptoTyped {
+                kind: alice_keypair.kind,
+                value: alice_keypair.value.key,
+            },
+        );
+
+        let msg_bytes = alice_crypto.encode(Request::Status).expect("encode");
+        let decoded = bob_crypto.decode::<Request>(&msg_bytes).expect("ok");
         assert_eq!(Request::Status, decoded);
         teardown_api(api).await;
     }
-
 }
